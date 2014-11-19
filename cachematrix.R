ba94bd520858604a90f0895c11d3de5b157d42eb
ronamaga <ronamaga@gmail.com>
2014-11-19 07:48:39 -0200

    uploading the Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,77 @@
-## Put comments here that give an overall description of what your
-## functions do
+##      Hello there,
 
-## Write a short comment describing this function
+##              Thanks for taking time to evaluate this assignment.
 
-makeCacheMatrix <- function(x = matrix()) {
+##              All the best!
 
+##      Ronald
+
+
+
+## When you create a matrix object with makeCacheMatrix(m), in which 'm' is a
+## square matrix with non-zero determinant (i.e. an invertible matrix), and call
+## 'cacheSolve(<the recently created matrix object>)' it will return 'm' inverted
+## either by calculating it using the solve() function or retrieving cached data
+## if the computing was already done. Pretty cool idea!!
+
+
+
+## This code creates a matrix object and  functions to i)store the matrix to be
+## inverted, ii)read it back, iii)calculate and iv)retrieve an inverted one.
+
+makeCacheMatrix <- function(z = matrix()) {
+        i <- NULL
+        definematrix <- function(w){
+                x <<- w
+                i <<- NULL
+        }
+        getmatrix <- function() z
+        defineinverted <- function(inverted) i <<- inverted
+        getinverted <- function() i
+        list(definematrix = definematrix, 
+             getmatrix = getmatrix, 
+             defineinverted = defineinverted, 
+             getinverted = getinverted)
 }
 
 
-## Write a short comment describing this function
+
+## The next function calls the matrix object created with the one above, checks if
+## the inverted matrix already exists and retrieves it, or calculates the inverted
+## matrix if it was done already. It also prompts a message if the matrix to be
+## inverted is either not square or has a zero determinant (non-invertible)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        i <- x$getinverted()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        thematrix <- x$getmatrix()
+
+        
+        ## This next 'if' checks if the matrix can be inverted:
+
+        
+        if((is.matrix(thematrix))&&(ncol(thematrix) == nrow(thematrix))&&!(det(thematrix) == 0)) {
+                i <- solve(thematrix,...)              
+        }else{
+                message("Not a matrix or the matrix is either not invertible or not square")
+                return(i)
+        }
+        
+        x$defineinverted(i)
+        i
 }
+
+## You may source the script above and try:
+
+## mymatrix <- makeCacheMatrix(matrix(runif(16),4,4))
+## cacheSolve(mymatrix)
+
+## and again:
+
+## cacheSolve(mymatrix)
+
+## to see it working
